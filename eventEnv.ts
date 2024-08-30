200880b77268aa8f853caa8b6c3bed607611b6e8
arjunkrishnagit <arjunkrishnaaju009@gmail.com>
2024-08-30 18:33:45 +0530

    Update eventEnv.ts

--- eventEnv.ts
+++ eventEnv.ts
@@ -1,4 +1,4 @@
 export const eventName = 'Dhanalakshmi-Group'
 export const lat = 10.541610424169638
 export const lon = 76.18434339028106
-export const caption = 'LICC by Hyatt Regency Thrissur'
+export const caption = 'LICC by Hyatt Regency, Thrissur'
